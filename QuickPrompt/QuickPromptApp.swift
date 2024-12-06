8eed1aa9323a2e49b5448a249f46b599f9ba61d4
Hans Poreda <hansporeda@icloud.com>
2024-12-06 12:19:40 +0100

    Initial Commit

---
+++ QuickPrompt/QuickPromptApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickPromptApp.swift
+//  QuickPrompt
+//
+//  Created by Hans Poreda on 06.12.24.
+//
+
+import SwiftUI
+
+@main
+struct QuickPromptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
